49789813e6bb249baf238cecfc2d56f96e4a8ecf
FereshteFallah <en.f.fallah@gmail.com>
2025-08-19 16:53:28 +0400

    Update settings and requirements for Railway deployment

--- smartshopping/settings.py
+++ smartshopping/settings.py
@@ -93,14 +93,25 @@ WSGI_APPLICATION = 'smartshopping.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import os
+import dj_database_url  
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
+
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / "db.sqlite3",
     }
 }
-
+'''
 '''
 DATABASES = {
     'default': {
